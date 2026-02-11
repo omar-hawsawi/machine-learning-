6e9dd2b967ba585ed022fd354b8ecab33f029900
omar-hawsawi <omarhaosaoe@gmail.com>
2026-02-11 10:24:49 +0300

    Add all project files

---
+++ lab 3.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+# to make the plote look cleaner
+df=pd.read_csv(r"C:\Users\hp\Desktop\ML lab 3\spambase dataset\spambase.data")
+print(df.head())
+print(df.shape)
